e4ea527d1d7d6ce4e62ab3437ace248e3bee71cb
Vinicius Cosendey <vicosendey@gmail.com>
2020-02-16 19:21:13 -0300

    Promises Chaining

--- 8-promises.js
+++ 8-promises.js
@@ -1,12 +1,30 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        //resolve([1,4,7])
-        reject('This is my error!')
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //resolve([1,4,7])
+//         reject('This is my error!')
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result)
-}).catch((error) => {
-    console.log('Error!', error)
+// doWorkPromise.then((result) => {
+//     console.log('Success!', result)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//Promises Chaining
+add(1, 2).then((sum) => {
+    console.log(sum)
+    return add(sum, 2)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
 })
